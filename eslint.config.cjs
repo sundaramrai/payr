d09e2cc83c93c1eb214db014c84b75a31d5d0800
Sundaram Rai <sundaramrai02@gmail.com>
2026-05-09 00:08:27 +0530

    refactor: simplify ESLint configuration by integrating core web vitals and removing unused settings

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,61 +1,14 @@
-const js = require('@eslint/js');
-const tsPlugin = require('@typescript-eslint/eslint-plugin');
-const tsParser = require('@typescript-eslint/parser');
+const nextVitals = require('eslint-config-next/core-web-vitals')
 
 module.exports = [
-    {
-        ignores: ['node_modules', '.next', '.next-dev', 'dist', 'build'],
+  ...nextVitals,
+  {
+    ignores: ['node_modules/**', '.next/**', '.next-dev/**', 'dist/**', 'build/**'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
-    js.configs.recommended,
-    {
-        files: ['**/*.ts', '**/*.tsx'],
-        languageOptions: {
-            parser: tsParser,
-            parserOptions: {
-                ecmaVersion: 2020,
-                sourceType: 'module',
-                ecmaFeatures: { jsx: true },
-            },
-            globals: {
-                window: 'readonly',
-                localStorage: 'readonly',
-                fetch: 'readonly',
-                Request: 'readonly',
-                Response: 'readonly',
-                AbortController: 'readonly',
-                setTimeout: 'readonly',
-                clearTimeout: 'readonly',
-                DOMException: 'readonly',
-                crypto: 'readonly',
-                React: 'readonly',
-                HTMLDialogElement: 'readonly',
-                HTMLHeadingElement: 'readonly',
-            },
-        },
-        plugins: { '@typescript-eslint': tsPlugin },
-        rules: {
-            'no-unused-vars': 'off',
-            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-        },
-    },
-    {
-        files: ['**/*.js', '**/*.jsx'],
-        languageOptions: {
-            parserOptions: {
-                ecmaVersion: 2020,
-                sourceType: 'module',
-                ecmaFeatures: { jsx: true },
-            },
-            globals: {
-                window: 'readonly',
-                localStorage: 'readonly',
-                fetch: 'readonly',
-                Request: 'readonly',
-                Response: 'readonly',
-                setTimeout: 'readonly',
-                clearTimeout: 'readonly',
-                React: 'readonly',
-            },
-        },
-    },
-];
+  },
+]
